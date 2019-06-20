006b8a12b80e5132ba89c42d16393910ac9d4f61
bhangun <bhangunh@gmail.com>
2019-06-20 16:37:00 +0700

    add simple theme

--- jh_flutter_mobx/lib/themes/light_theme.dart
+++ jh_flutter_mobx/lib/themes/light_theme.dart
@@ -1,46 +1,26 @@
 import 'package:flutter/material.dart';
 
+import 'material_theme_colors.dart';
+
 
 
 final ThemeData lightTheme = _buildTheme();
 
-const kShrinePink50 = Color(0xFFFEEAE6);
-const kShrinePink100 =  Color(0xFFFEDBD0);
-const kShrinePink300 =  Color(0xFFFBB8AC);
-const kShrinePink400 =  Color(0xFFEAA4A4);
-const kShrineBrown900 =  Color(0xFF442B2D);
-const kShrineErrorRed =  Color(0xFFC5032B);
-const kShrineSurfaceWhite =  Color(0xFFFFFBFA);
-const kShrineBackgroundWhite = Colors.white;
-
- const Map<int, Color> green = const <int, Color>{
-    50: const Color(0xFFf2f8ef),
-    100: const Color(0xFFdfedd8),
-    200: const Color(0xFFc9e2be),
-    300: const Color(0xFFb3d6a4),
-    400: const Color(0xFFa3cd91),
-    500: const Color(0xFF93c47d),
-    600: const Color(0xFF8bbe75),
-    700: const Color(0xFF80b66a),
-    800: const Color(0xFF76af60),
-    900: const Color(0xFF64a24d)
-  };
-
 ThemeData _buildTheme() {
   final ThemeData base = ThemeData.light();
   return base.copyWith(
-    accentColor: kShrineBrown900,
-    primaryColor: kShrinePink100,
-    buttonColor: kShrinePink100,
-    scaffoldBackgroundColor: kShrineBackgroundWhite,
-    cardColor: kShrineBackgroundWhite,
-    textSelectionColor: kShrinePink100,
-    errorColor: kShrineErrorRed,
+    accentColor: MatThemeColors.blue[800],
+    primaryColor: MatThemeColors.blue[100],
+    buttonColor: MatThemeColors.blue[200],
+    scaffoldBackgroundColor: MatThemeColors.blue[50],
+    cardColor: MatThemeColors.blue[50],
+    textSelectionColor: MatThemeColors.blue[200],
+    errorColor: MatThemeColors.deepPurple[200],
     buttonTheme: ButtonThemeData(
       textTheme: ButtonTextTheme.accent,
     ),
     primaryIconTheme: base.iconTheme.copyWith(
-        color: kShrineBrown900
+        color: MatThemeColors.deepPurple[100]
     ),
     textTheme: _buildTextTheme(base.textTheme),
     primaryTextTheme: _buildTextTheme(base.primaryTextTheme),
@@ -65,8 +45,8 @@ TextTheme _buildTextTheme(TextTheme base) {
       fontSize: 16.0,
     ),
   ).apply(
-    fontFamily: 'Rubik',
-    displayColor: kShrineBrown900,
-    bodyColor: kShrineBrown900,
+    fontFamily: 'ProductSans',
+    displayColor: MatThemeColors.blue[800],
+    bodyColor: MatThemeColors.blue[800],
   );
 }
